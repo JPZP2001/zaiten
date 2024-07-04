fceed11d683962cf41970ac500e61a68431421ac
JP <juan.zubieta@udem.edu>
2024-07-04 01:24:30 -0600

    fix setup

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,3 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  base: "/zaiten/",
-  plugins: [],
-  resolve: {
-    alias: [
-      
-    ]
-  },
-  transpileDependencies: true
-})
-
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/zaiten/'
-    : '/'
+  publicPath: '/zaiten/'
 }
